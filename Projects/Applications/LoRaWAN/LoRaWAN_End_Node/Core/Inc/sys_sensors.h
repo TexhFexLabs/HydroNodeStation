f8f6b4bc9886b49a85bd766b3db71d1e3cdbedd9
Felix Knoll <email@knollfelix.de>
2026-03-27 10:31:35 +0100

    Initial commit

---
+++ Projects/Applications/LoRaWAN/LoRaWAN_End_Node/Core/Inc/sys_sensors.h
@@ -0,0 +1,91 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    sys_sensors.h
+  * @author  MCD Application Team
+  * @brief   Header for sensors application
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __SENSORS_H__
+#define __SENSORS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/**
+  * Sensor data parameters
+  */
+typedef struct
+{
+  float temperature;      /*!< in degC */
+  float humidity;         /*!< in % */
+  uint16_t co2;           /*!< in ppm */
+  /* USER CODE BEGIN sensor_t */
+
+  /* USER CODE END sensor_t */
+} sensor_t;
+
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* External variables --------------------------------------------------------*/
+/* USER CODE BEGIN EV */
+
+/* USER CODE END EV */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+/**
+  * @brief  initialize the environmental sensor
+  */
+int32_t EnvSensors_Init(void);
+
+/**
+  * @brief  read environmental sensor data
+  * @param  sensor_data pointer to sensor data struct
+  */
+int32_t EnvSensors_Read(sensor_t *sensor_data);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SENSORS_H__ */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Projects/Applications/LoRaWAN/LoRaWAN_End_Node/Core/Src/sys_sensors.c
@@ -0,0 +1,90 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    sys_sensors.c
+  * @author  MCD Application Team
+  * @brief   Manages the sensors on the application
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stdint.h"
+#include "sys_conf.h"
+#include "sys_sensors.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* External variables ---------------------------------------------------------*/
+/* USER CODE BEGIN EV */
+
+/* USER CODE END EV */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Exported functions --------------------------------------------------------*/
+int32_t EnvSensors_Read(sensor_t *sensor_data)
+{
+  /* USER CODE BEGIN EnvSensors_Read */
+  sensor_data->temperature = 0.0f;
+  sensor_data->humidity    = 0.0f;
+  sensor_data->co2         = 0;
+
+  return 0;
+  /* USER CODE END EnvSensors_Read */
+}
+
+int32_t EnvSensors_Init(void)
+{
+  /* USER CODE BEGIN EnvSensors_Init */
+
+  return 0;
+  /* USER CODE END EnvSensors_Init */
+}
+
+/* USER CODE BEGIN EF */
+
+/* USER CODE END EF */
+
+/* Private Functions Definition -----------------------------------------------*/
+/* USER CODE BEGIN PrFD */
+
+/* USER CODE END PrFD */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
